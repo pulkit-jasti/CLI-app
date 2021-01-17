81302be37a7affaa103fba724f7ea9d490d06c2f
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-01-17 17:05:22 +0530

    Song search command working and displaying meta data

--- bin/songdex.js
+++ bin/songdex.js
@@ -6,31 +6,17 @@ const request = require('request');
 const cheerio = require('cheerio');
 const inquirer = require('inquirer');
 const colors = require('colors');
+const fetch = require('node-fetch');
 
 console.log('song dex link complete');
-
 
 commander
 	.version(pkg.version)
 	.option('-sn, --songName [name of song]', 'Search for lyrics of any song')
-	.action((par)=>{
+	.action(par => {
 		console.log(par.songName);
 		console.log('--------'.blue);
-		do{
-			songSearch(par.songName)
-		}
-		while(par.songName=='')
-		console.log('---------'.green);
-		/*const input = inquirer.prompt([
-			{
-				type: 'input',
-				name: 'sampleInput',
-				message: 'Enter some random message2'.red
-			}
-		])
-
-		console.log(input.sampleInput);
-		*/
+		songSearch(par.songName);
 	})
 	.parse(process.argv);
 
@@ -39,6 +25,11 @@ commander
 const options = commander.opts();
 //console.log(options.songName);
 
+function line(num) {
+	for (let i = 0; i < num; i++) {
+		console.log('============================================================='.yellow);
+	}
+}
 
 //songSearch('zen zen zense')
 
@@ -58,17 +49,52 @@ function songSearch(songName) {
 		if (error) throw new Error(error);
 
 		const data = JSON.parse(body);
-		//console.log(data.response.hits[0].result.path);
-		getLyrics(data.response.hits[0].result.path);
+		line(1);
+		console.log('SEARCH RESULTS'.green);
+		line(1);
+		for (let i = 0; i < 10; i++) {
+			console.log(`${i + 1}. ${data.response.hits[i].result.title}`);
+		}
+
+		console.log('\n');
+		inquirer
+			.prompt([
+				{
+					type: 'input',
+					name: 'searchIndex',
+					message: 'Select an option'.red,
+				},
+			])
+			.then(cb => {
+				let index = parseInt(cb.searchIndex, 10) - 1;
+				getSongInfo(data.response.hits[index].result);
+			})
+			.catch(err => console.log(err));
+
+		return data;
 	});
 }
 
-function getLyrics(lyricsURL) {
-	request(`https://genius.com${lyricsURL}`, (error, res, html) => {
-		if (!error && res.statusCode == 200) {
-			const $ = cheerio.load(html);
+function getSongInfo(obj) {
+	console.log('\n\n==============='.red);
+	console.log('TITLE'.blue);
+	console.log('===============\n'.red);
+	console.log(obj.title);
+
+	console.log('\n\n==============='.red);
+	console.log('ARTIST'.blue);
+	console.log('===============\n'.red);
+	console.log(obj.primary_artist.name);
+
+	console.log('\n\n==============='.red);
+	console.log('LYRICS'.blue);
+	console.log('===============\n'.red);
+	fetch(`https://genius.com${obj.path}`)
+		.then(res => res.text())
+		.then(data => {
+			const $ = cheerio.load(data);
 			const lyrics = $('.lyrics>p');
 			console.log(lyrics.text());
-		}
-	});
+		})
+		.catch(err => console.log(err));
 }
